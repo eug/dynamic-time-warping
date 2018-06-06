3d31514b183ead4c0870721d82438730b57e15c5
Eugenio Cabral <eugfcl@gmail.com>
2018-06-06 15:45:25 -0300

    Add dtw implementations

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 from utils.io import read_input_2d, read_input_3d
 import sys
-
+from tqdm import tqdm
 
 
 def matrix(n, m=None, default=0):
@@ -9,27 +9,73 @@ def matrix(n, m=None, default=0):
     return [[default] * m for _ in range(n)]
 
 
-def dynamic_time_warping(s, t):
+def dtw_naive(s, t, prune_score=math.inf):
     n, m = len(s), len(t)
-    dtw = matrix(n, m)
-
-    for i in range(n):
-        dtw[i][0] = math.inf
-    for i in range(m):
-        dtw[0][i] = math.inf
+    dtw = matrix(n, m, default=math.inf)
     dtw[0][0] = 0
+    min_row_score = -1
 
-    for i in range(n):
-        for j in range(m):
-            dist = abs(s[i] - t[j])
-            dtw[i][j] = dist + min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
-            sys.stdout.write(str(dtw[i][j]) + " ")
-        print('')
+    for i in range(1, n):
+        min_row_score = math.inf
 
-    return dtw[n-1][m-1]
+        for j in range(1, m):
+            dtw[i][j] = abs(s[i] - t[j]) + min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
+            # min_row_score = min(min_row_score, dtw[i][j])
+
+        if min_row_score > prune_score:
+            return min_row_score
+    
+    return min_row_score
 
 
-train, test, labels = read_input_3d()
-dynamic_time_warping([1,2,3,4,5,6],[1,2,3,4,5,6])
+def dtw_sakoe_chiba(s, t, w, prune_score=math.inf):
+    n, m = len(s), len(t)
+    w = max(w, abs(n - m) + 1)
+    dtw = matrix(n, m, default=math.inf)
+    dtw[0][0] = 0
+    min_row_score = -1
+
+    for i in range(1, n):
+        min_row_score = math.inf
+
+        for j in range(max(1, i-w), min(m, i+w)):
+            dtw[i][j] = abs(s[i] - t[j]) + min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
+            min_row_score = min(min_row_score, dtw[i][j])
+
+        if min_row_score > prune_score:
+            return min_row_score
+
+    return min_row_score
 
 
+def predict(train, test):
+    y_pred = []
+    for tsl, t in tqdm(test):
+        min_label, min_score = -1, math.inf
+        for trl, s in train:
+            # score = dtw_sakoe_chiba(s, t, 5, min_score)
+            score = dtw_naive(s, t, min_score)
+            if score < min_score:
+                min_score = score
+                min_label = trl
+        y_pred.append(tsl == min_label)
+    
+    return y_pred
+
+
+train, test, labels = read_input_2d()
+y_pred = predict(train, test)
+print(sum(y_pred))
+print(len(y_pred))
+print( sum(y_pred) / len(y_pred) * 100 )
+
+# print(len(train[0][1]), len(test[0][1]))
+# n, m = len(train[0][1]), len(test[0][1])
+# dtw = dtw_naive(train[0][1], test[0][1])
+# dtw = dtw_sakoe_chiba(train[0][1], test[0][1], 5)
+# print( dtw[len(train[0][1])-1][len(test[0][1])-1] )
+# print(dtw)
+# for i in range(n):
+#     for j in range(m):
+#         sys.stdout.write(str(dtw[i][j]) + "\t")
+#     sys.stdout.write("\n")
