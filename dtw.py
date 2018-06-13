bd0dae351a8871f1e9fe1482227f99e0d9935df7
Eugênio Cabral <eugfcl@gmail.com>
2018-06-13 14:55:58 -0300

    Fix DTW minor bugs

--- dtw.py
+++ dtw.py
@@ -5,51 +5,32 @@ def matrix(n, m=None, default=0):
     return [[default] * m for _ in range(n)]
 
 def dtw_naive(s, t, dist, prune_score=None):
-    min_row_score = -1
     n, m = len(s), len(t)
-    dtw = matrix(n, m)
+    dtw = matrix(n, m, default=math.inf)
+    dtw[0][0] = 0
 
-    for i in range(n):
-        min_row_score = math.inf
-        for j in range(m):
-
-            min_prev_cost = 0
-            if i > 0 and j > 0:
-                min_prev_cost = min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
-            elif i > 0 and j == 0:
-                min_prev_cost = dtw[i-1][j]
-            elif i == 0 and j > 0:
-                min_prev_cost = dtw[i][j-1]
-            
+    for i in range(1, n):
+        for j in range(1, m):
             cost = dist(s[i], t[j])
-            dtw[i][j] = cost + min_prev_cost
-            min_row_score = min(min_row_score, dtw[i][j])
+            dtw[i][j] = cost + min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
 
-        if prune_score and min_row_score > prune_score:
-            return min_row_score
+        if prune_score and min(dtw[i]) > prune_score:
+            return math.inf
 
-    return min_row_score
-
+    return dtw[-1][-1]
 
 def dtw_sakoe_chiba(s, t, w, dist, prune_score=None):
-    min_row_score = -1
     n, m = len(s), len(t)
     w = max(w, abs(n - m) + 1)
     dtw = matrix(n, m, default=math.inf)
+    dtw[0][0] = 0
 
-    for i in range(n):
-        min_row_score = math.inf
-        for j in range(max(0, i-w), min(m, i+w)):
-
-            min_prev_cost = 0
-            if i != 0 or j != 0:
-                min_prev_cost = min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
-
+    for i in range(1, n):
+        for j in range(max(1, i-w), min(m, i+w)):
             cost = dist(s[i], t[j])
-            dtw[i][j] = cost + min_prev_cost
-            min_row_score = min(min_row_score, dtw[i][j])
+            dtw[i][j] = cost + min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
 
-        if prune_score and min_row_score > prune_score:
-            return min_row_score
+        if prune_score and min(dtw[i]) > prune_score:
+            return math.inf
 
-    return min_row_score
+    return dtw[-1][-1]
